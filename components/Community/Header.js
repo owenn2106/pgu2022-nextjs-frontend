48d20c476644c1a8fb1eea147ea7bef332107e0e
Owenn Gimli <owenn2106@icloud.com>
2022-07-17 21:33:14 +0700

    added header background

--- components/Community/Header.js
+++ components/Community/Header.js
@@ -1,5 +1,27 @@
+import styles from "../../styles/Community/Header.module.css";
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className={styles.HeaderWrapper}>
+      <div className={styles.Navigation}>
+        <ul>
+          <li>
+            <a href=""></a>SAC &amp; SCC
+          </li>
+          <li>
+            <a href=""></a>HIMA
+          </li>
+          <li>
+            <a href=""></a>ORGANIZATION
+          </li>
+        </ul>
+      </div>
+      <div className={styles.Title}>
+        <h1>COMMUNITY</h1>
+        <h3>LIFE</h3>
+      </div>
+    </div>
+  );
 };
 
 export default Header;

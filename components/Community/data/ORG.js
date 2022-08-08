dbc6041005c1425533f9dcc1c9c28ab2cba03ae7
Owenn Gimli <owenn2106@icloud.com>
2022-08-08 12:57:29 +0800

    shorten AIESEC text

--- components/Community/data/ORG.js
+++ components/Community/data/ORG.js
@@ -18,9 +18,9 @@ export const ORG = [
     logo: "/images/logo-org/aiesec.png",
     description: "Leadership and global volunteer exchange program",
     body1:
-      "AIESEC in Prasetiya Mulya is a not-for-profit organization striving for peace and fulfillment of humankind's potential by developing youth leadership. We provide youth to develop their leadership through cross-cultural internships and global volunteer exchange experiences. Since 2011, AIESEC in Prasetiya Mulya has created a lot of outstanding leaders, as many as 400 individuals were sent to several countries. Global Volunteer is an exchange program intended to shape the youth to become the best version of themselves and activate their leadership skills.",
+      "AIESEC in Prasetiya Mulya is a non-profit organization striving for peace and fulfillment of humankind's potential by developing youth leadership. Members develop their leadership through cross-cultural internships and global volunteer exchange experiences. Since 2011, AIESEC in Prasetiya Mulya has created a lot of outstanding leaders, as many as 400 individuals were sent to several countriest through Global Volunteer which is an exchange program.",
     body2:
-      "AIESEC in Prasetiya Mulya is for every Prasmulyans that are seeking to develop their leadership skills and also their soft and hard skills, while at the same time positively impacting the world.",
+      "AIESEC in Prasetiya Mulya is for every Prasmulyans that are seeking to develop their leadership, soft and hard skills, while at the same time positively impacting the world.",
     cp_name: "Vincent Zhuang",
     cp_line: "dream3r",
     cp_wa: "081388095869",

12c698fb52adab319df56c12cacc3088570f9261
Owenn Gimli <owenn2106@icloud.com>
2022-08-16 16:36:31 +0800

    updated daily recap for day 5

--- components/Home/Recap.js
+++ components/Home/Recap.js
@@ -14,48 +14,46 @@ const Recap = () => {
         <div className={styles.recapHeader}>
           <div className={styles.dateWrapper}>
             <h2>Daily Recap</h2>
-            <h3>15 August 2022</h3>
+            <h3>16 August 2022</h3>
           </div>
           <div className={styles.valueWrapper}>
             <div>
-              <p>Analytical</p>
-              <p>Thinking</p>
+              <p>Maturity</p>
+              {/* <p>Thinking</p> */}
             </div>
             <hr />
-            <p>Leadership</p>
+            <p>Achieving</p>
           </div>
         </div>
         <div className={styles.recapContent}>
           <div className={styles.recapTitle}>
-            <h3>Surviv(or)loss</h3>
+            <h3>All Hands on Deck</h3>
           </div>
           <div className={styles.recapBody}>
             <p>
-              On the fourth day, two values are emphasized, Analytical Thinking
-              and Leadership, through speaker sessions brought by Alamanda
-              Shantika and Christian Sugiono. Alamanda Shantika shared the
-              importance of Analytical thinking with the topic, &quot;Building a
-              Thorough Way of Thinking&quot;. She recalled analytical thinking
-              as a way to search for opportunities and to view from another
-              perspective. While Christian Sugiono equipped us with Leadership
-              skills with his topic &quot;Building Trust and Respect&quot;. He
-              mentioned leadership as a skill to encourage others to achieve
-              goals. As a student, those skills help us to achieve tasks, build
-              relationships, and decision making.
+              The fifth day of Prasmul Gear up 2022 focused on maturity and
+              achieving. Today&apos;s sessions were brought by Titan Tyra, Barry
+              Likumahuwa, and Greysia Polii. As a content creator and
+              entrepreneur, Titan showed us what it means to be mature by
+              conquering ourselves and building a positive mindset. She
+              mentioned self-awareness as a need for people to progress and
+              develop. Also, by being in the music industry, Barry&apos;s
+              understanding of maturity taught us the importance of maximizing
+              opportunities to enhance ourselves.
             </p>
             <br />
             <p>
-              Voyagers also attended the Scavenger Hunt. Through this session,
-              Voyagers learned how to cooperate with moral standards as a group.
-              To increase their understanding as a freshman, they must answer
-              some questions regarding campus life through some interviews with
-              Prasmul Gear Up committees.
+              Meanwhile, Greysia Polii told us about her struggles as an athlete
+              and one of her sayings that could be quoted is “Achieving
+              something is good, but we have to be careful with what we are
+              going to achieve”. This implies that we have to set objective(s)
+              carefully to achieve something.
             </p>
             <br />
             <p>
-              By this chance, we hope voyagers could understand the importance
-              of Analytical Thinking and Leadership and implement it in their
-              daily life.
+              Through this opportunity, hopefully voyagers could learn the
+              significance of Maturity and Achieving and also be able to put it
+              into their everyday lives.
             </p>
           </div>
         </div>
